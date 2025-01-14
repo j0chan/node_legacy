927d76f1f1ca047651e59adb3bb43d26d0772740
Youngchan <wdc4512@naver.com>
2025-01-14 11:16:33 +0900

    chore: 메인 어플리케이션이 바디파서를 사용하도록 설정

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+// 라이브러리 설치 후 사용할 수 있도록 변수에 할당
 const express = require('express')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-
 app.set('view engine', 'ejs')
 app.set('views', './views')
-// static file serving (정적파일 서빙, 보통 public에 저장)
-app.use(express.static(__dirname + '/public'))
-
+app.use(express.static(__dirname + '/public')) // static file serving (정적파일 서빙, 보통 public에 저장)
+app.use(bodyParser.urlencoded({extended: false})) // parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // parsing JSON
 
 // 라우팅 하는 곳
 // localhost:3000 을 통해 접속
